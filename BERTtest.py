083970d23ce009673d812bf4693fba0ecc45f7da
NinaTabari <69057253+NinaTabari@users.noreply.github.com>
2023-06-11 17:50:11 -0400

    Create BERTtest.py

---
+++ BERTtest.py
@@ -0,0 +1,96 @@
+# Import necessary libraries
+from transformers import BertTokenizer, TFBertForSequenceClassification
+from transformers import InputExample, InputFeatures
+import tensorflow as tf
+import pandas as pd
+
+# Load the tokenizer
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+# Prepare the data
+# Assuming we have the dataset stored in two CSV files, one for training and one for testing
+train_data = pd.read_csv("train.csv") 
+test_data = pd.read_csv("test.csv")
+
+# Convert the data into the appropriate format
+def convert_data_to_examples(train, test, DATA_COLUMN, LABEL_COLUMN): 
+    train_InputExamples = train.apply(lambda x: InputExample(guid=None, 
+        text_a = x[DATA_COLUMN], 
+        text_b = None,
+        label = x[LABEL_COLUMN]), axis = 1)
+
+    validation_InputExamples = test.apply(lambda x: InputExample(guid=None, 
+        text_a = x[DATA_COLUMN], 
+        text_b = None,
+        label = x[LABEL_COLUMN]), axis = 1)
+
+    return train_InputExamples, validation_InputExamples
+
+def convert_examples_to_tf_dataset(examples, tokenizer, max_length=128):
+    features = [] 
+    for e in examples:
+        input_dict = tokenizer.encode_plus(
+            e.text_a,
+            add_special_tokens=True,
+            max_length=max_length, # truncates if len(s) > max_length
+            return_token_type_ids=True,
+            return_attention_mask=True,
+            pad_to_max_length=True, # pads to the right by default
+            truncation=True
+        )
+
+        input_ids, token_type_ids, attention_mask = (input_dict["input_ids"],
+            input_dict["token_type_ids"], input_dict['attention_mask'])
+
+        features.append(
+            InputFeatures(
+                input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, label=e.label
+            )
+        )
+
+    def gen():
+        for f in features:
+            yield (
+                {
+                    "input_ids": f.input_ids,
+                    "attention_mask": f.attention_mask,
+                    "token_type_ids": f.token_type_ids,
+                },
+                f.label,
+            )
+
+    return tf.data.Dataset.from_generator(
+        gen,
+        ({"input_ids": tf.int32, "attention_mask": tf.int32, "token_type_ids": tf.int32}, tf.int64),
+        (
+            {
+                "input_ids": tf.TensorShape([None]),
+                "attention_mask": tf.TensorShape([None]),
+                "token_type_ids": tf.TensorShape([None]),
+            },
+            tf.TensorShape([]),
+        ),
+    )
+
+DATA_COLUMN = 'text'
+LABEL_COLUMN = 'label'
+
+train_InputExamples, validation_InputExamples = convert_data_to_examples(train_data, test_data, DATA_COLUMN, LABEL_COLUMN)
+
+train_data = convert_examples_to_tf_dataset(list(train_InputExamples), tokenizer)
+train_data = train_data.shuffle(100).batch(32).repeat(2)
+
+validation_data = convert_examples_to_tf_dataset(list(validation_InputExamples), tokenizer)
+validation_data = validation_data.batch(32)
+
+# Create the model
+model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
+
+# Compile the model
+optimizer = tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
+model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+
+# Train the model
+model.fit(train_data, epochs=2, validation_data=validation_data)
